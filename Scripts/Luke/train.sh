f8ee6a5c5e026c1cbd2ed35e85d4675d3d74bbd0
Luke Smith <lusmith@lo-login-01.leonhard.ethz.ch>
2021-05-28 11:27:05 +0200

    New trainings

--- Scripts/Luke/train.sh
+++ Scripts/Luke/train.sh
@@ -1,6 +1,11 @@
-# if [ -z $1 ]
-# then
-#     echo "Specify a username as argument"
-# fi
-bsub -n 1 -W 1:00 -o /cluster/home/lusmith/MachinePerception/training/log_train -R "rusage[mem=4096, ngpus_excl_p=1]" python main.py /cluster/project/infk/hilliges/lectures/mp21/project6/dataset --dataset "humanflow" --name '/cluster/home/lusmith/MachinePerception/training' --pretrained /cluster/home/lusmith/BananaPyjama/pretrained/pwc_MHOF.pth.tar
+PYTHON_CMD="python main.py 
+    /cluster/project/infk/hilliges/lectures/mp21/project6/dataset 
+    --dataset humanflow
+    --name /cluster/home/lusmith/MachinePerception/training/test_training
+    --pretrained /cluster/home/lusmith/BananaPyjama/pretrained/pwc_MHOF.pth.tar
+    --epochs 5"
+    # --epoch_size 200
+    # --batch-size 10
+
+bsub -n 1 -W 10:00 -o /cluster/home/lusmith/MachinePerception/training/multi_epoch_training/logs/log_train -R "rusage[mem=4096, ngpus_excl_p=1]" $PYTHON_CMD
 watch -n 1 bjobs
