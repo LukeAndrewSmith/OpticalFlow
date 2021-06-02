26b1d6802906960780d4b610fa777d6467818de9
Luke Smith <lusmith@lo-login-01.leonhard.ethz.ch>
2021-06-02 10:39:39 +0200

    Updated scripts

--- Scripts/Luke/train.sh
+++ Scripts/Luke/train.sh
@@ -1,11 +1,10 @@
 PYTHON_CMD="python main.py 
     /cluster/project/infk/hilliges/lectures/mp21/project6/dataset 
     --dataset humanflow
-    --name /cluster/home/lusmith/MachinePerception/training/test_training
+    --name /cluster/home/lusmith/MachinePerception/training/fine_tune_27
     --pretrained /cluster/home/lusmith/BananaPyjama/pretrained/pwc_MHOF.pth.tar
-    --epochs 5"
-    # --epoch_size 200
-    # --batch-size 10
+    --epochs 27
+    --batch-size 10"
 
-bsub -n 1 -W 10:00 -o /cluster/home/lusmith/MachinePerception/training/multi_epoch_training/logs/log_train -R "rusage[mem=4096, ngpus_excl_p=1]" $PYTHON_CMD
+bsub -n 1 -W 24:00 -o /cluster/home/lusmith/MachinePerception/training/fine_tune_27/logs/log_train -R "rusage[mem=4096, ngpus_excl_p=1]" $PYTHON_CMD
 watch -n 1 bjobs
--- Scripts/Luke/visuals.sh
+++ Scripts/Luke/visuals.sh
@@ -1,2 +1,8 @@
-# python /cluster/home/lusmith/BananaPyjama/generate_visuals.py /cluster/project/infk/hilliges/lectures/mp21/project6/dataset --phase test --pred-dir /cluster/home/lusmith/MachinePerception/visuals/output/results-sample-baseline-leonhard --output-dir /cluster/home/lusmith/MachinePerception/visuals/test
-python /cluster/home/lusmith/BananaPyjama/generate_visuals.py /cluster/project/infk/hilliges/lectures/mp21/project6/dataset --phase test --output-dir /cluster/home/lusmith/MachinePerception/visuals/full_set
+NAME=fine_tune_27
+
+python /cluster/home/lusmith/BananaPyjama/generate_visuals.py \
+    /cluster/project/infk/hilliges/lectures/mp21/project6/dataset \
+    --phase test \
+    --pred-dir /cluster/home/lusmith/MachinePerception/submission/fine_tune_27 \
+    --output-dir /cluster/home/lusmith/MachinePerception/visuals/fine_tune_27
+# python /cluster/home/lusmith/BananaPyjama/generate_visuals.py /cluster/project/infk/hilliges/lectures/mp21/project6/dataset --phase test --output-dir /cluster/home/lusmith/MachinePerception/visuals/full_set
